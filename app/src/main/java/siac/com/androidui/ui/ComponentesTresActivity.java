19f31da3875fe2b3a9d361c8ca4717bb6bc90a73
aureo <concyline@hotmail.com>
2021-03-24 16:37:57 -0300

    Finish CustonDialog

--- app/src/main/java/siac/com/androidui/ui/ComponentesTresActivity.java
+++ app/src/main/java/siac/com/androidui/ui/ComponentesTresActivity.java
@@ -2,9 +2,15 @@ package siac.com.androidui.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import siac.com.androidui.MainActivity;
 import siac.com.androidui.R;
+import siac.com.componentes.CustomDialog;
 
 public class ComponentesTresActivity extends AppCompatActivity {
 
@@ -12,5 +18,49 @@ public class ComponentesTresActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_componentes_tres);
+
+        findViewById(R.id.button12).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+
+                    CustomDialog customDialog = new CustomDialog(ComponentesTresActivity.this);
+                    customDialog.setContentView(R.layout.cadastro)
+                            .setToolbarTitle("Log IN")
+                            .setToolbarSubTitle("Enter the system")
+                            .setMenuToolbar(R.menu.menu_bar)
+                            .setBackgroundResource(CustomDialog.DWindow.ROUND)
+                            .setHeight(CustomDialog.DLayoutParams.WRAP_CONTENT)
+                            .setCancelable(true)
+                            .create();
+
+                    customDialog.menu(R.id.miCompose).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            System.out.println("MENU");
+                            return false;
+                        }
+                    });
+
+                    Dialog dialog = customDialog.dialog();
+
+                    Button button = dialog.findViewById(R.id.button);
+                    button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            System.out.println("BUTTON");
+                        }
+                    });
+
+
+                    customDialog.show();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
