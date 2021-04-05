8e32f66d0ec3f56212b4e1b0896c3c7a830bd604
aureo <concyline@hotmail.com>
2021-04-05 16:50:05 -0300

    Fim do projeto RecyclerButton

--- app/src/main/java/siac/com/androidui/ui/ComponentesTresActivity.java
+++ app/src/main/java/siac/com/androidui/ui/ComponentesTresActivity.java
@@ -1,18 +1,33 @@
 package siac.com.androidui.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.app.Dialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import siac.com.androidui.MainActivity;
 import siac.com.androidui.R;
 import siac.com.componentes.CustomDialog;
+import siac.com.componentes.RecyclerViewButton;
 
 public class ComponentesTresActivity extends AppCompatActivity {
+
+    private List<Cidade> lCidade = new ArrayList<>();
+    private RecyclerViewButton recyclerViewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,5 +84,144 @@ public class ComponentesTresActivity extends AppCompatActivity {
 
             }
         });
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        recyclerViewButton = findViewById(R.id.recyclerViewButton);
+
+        new Async().execute();
+
+        recyclerViewButton.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Async().execute();
+            }
+        });
+
+        recyclerViewButton.addOnItemTouchListener(new RecyclerViewButton.ItemClickListener(new RecyclerViewButton.Listener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                System.out.println("aqui");
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+                System.out.println("aqui");
+            }
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println("aqui");
+            }
+        }));
+    }
+
+    public class Async extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            recyclerViewButton.setRefreshing(true);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            contruct();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            recyclerViewButton.setRefreshing(false);
+        }
+    }
+
+    private void contruct(){
+
+        for(int i = 0; i < 50; i++) {
+            lCidade.add(new Cidade("GOIANIA", "GO"));
+            lCidade.add(new Cidade("BRAZABRANTES", "GO"));
+            lCidade.add(new Cidade("INHUMAS", "GO"));
+            lCidade.add(new Cidade("ITAU", "GO"));
+        }
+
+        recyclerViewButton.setAdapter(this, new Adapter());
+    }
+
+    public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        private LayoutInflater mInflater;
+
+        Adapter() {
+            this.mInflater = LayoutInflater.from(getBaseContext());
+        }
+
+        @Override
+        @NonNull
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = mInflater.inflate(R.layout.cidade_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+            final ViewHolder viewHolder = (ViewHolder) holder;
+
+            Cidade cidade = lCidade.get(position);
+
+            viewHolder.nomeTextView.setText(cidade.getNome());
+            viewHolder.ufTextView.setText(cidade.getUf());
+        }
+
+        @Override
+        public int getItemCount() {
+            return lCidade.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView nomeTextView;
+            TextView ufTextView;
+
+            ViewHolder(View itemView) {
+                super(itemView);
+                nomeTextView = itemView.findViewById(R.id.nomeTextView);
+                ufTextView = itemView.findViewById(R.id.ufTextView);
+            }
+        }
+
+        Cidade getItem(int id) {
+            return lCidade.get(id);
+        }
+    }
+
+    public class Cidade {
+
+        private String nome;
+        private String uf;
+
+        public Cidade(String nome, String uf) {
+            this.nome = nome;
+            this.uf = uf;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getUf() {
+            return uf;
+        }
+
+        public void setUf(String uf) {
+            this.uf = uf;
+        }
     }
 }
---
+++ componentes/src/main/java/siac/com/componentes/RecyclerViewButton.java
@@ -0,0 +1,225 @@
+package siac.com.componentes;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+public class RecyclerViewButton extends FrameLayout {
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private static RecyclerView recyclerView;
+    private ImageView goToTopImageView;
+
+    private static Context context;
+    private int locationButton;
+    private int numberOfColumns;
+    private boolean horizontalDivider;
+    private boolean verticalDivider;
+
+    public RecyclerViewButton(@NonNull Context context) {
+        super(context);
+        obtainStyledAttributes(context, null, 0);
+        init();
+    }
+
+    public RecyclerViewButton(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        obtainStyledAttributes(context, attrs, 0);
+        init();
+    }
+
+    public RecyclerViewButton(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        obtainStyledAttributes(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void obtainStyledAttributes(Context context, AttributeSet attrs, int defStyleAttr) {
+
+        if (attrs != null) {
+            TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.RecyclerViewButton, defStyleAttr, 0);
+
+            locationButton = typedArray.getInteger(R.styleable.RecyclerViewButton_locationButton, 0);
+            numberOfColumns = typedArray.getInteger(R.styleable.RecyclerViewButton_numberOfColumns, 1);
+            horizontalDivider = typedArray.getBoolean(R.styleable.RecyclerViewButton_horizontalDivider, false);
+            verticalDivider = typedArray.getBoolean(R.styleable.RecyclerViewButton_verticalDivider, false);
+            return;
+        }
+
+    }
+
+    private void init() {
+        inflate(getContext(), R.layout.view_recyclerview_button_ui, this);
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        goToTopImageView = findViewById(R.id.goToTopImageView);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        setup();
+    }
+
+    private void setup() {
+
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) goToTopImageView.getLayoutParams();
+
+        if (locationButton == 1) {
+            params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        } else if (locationButton == 2) {
+            params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        } else {
+            params.addRule(RelativeLayout.TEXT_ALIGNMENT_CENTER);
+        }
+
+        goToTopImageView.setLayoutParams(params);
+
+        goToTopImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fadeIn(v);
+                recyclerView.smoothScrollToPosition(0);
+            }
+        });
+
+        recyclerView.addOnScrollListener(onScrollListener);
+    }
+
+    RecyclerView.OnScrollListener onScrollListener = new RecyclerView.OnScrollListener() {
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+        }
+
+        @Override
+        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+            super.onScrollStateChanged(recyclerView, newState);
+
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_FLING) {
+                Animation in = AnimationUtils.loadAnimation(context, R.anim.fadeout_lento);
+                goToTopImageView.startAnimation(in);
+                goToTopImageView.setVisibility(View.INVISIBLE);
+
+            } else if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                goToTopImageView.setVisibility(View.VISIBLE);
+            }
+        }
+    };
+
+    public void setAdapter(final Activity context, @Nullable final RecyclerView.Adapter adapter) {
+        this.context = context;
+
+        context.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, numberOfColumns));
+
+                if (horizontalDivider) {
+                    recyclerView.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL));
+                }
+
+                if (verticalDivider) {
+                    recyclerView.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL));
+                }
+
+                recyclerView.setAdapter(adapter);
+            }
+        });
+    }
+
+    public void setOnRefreshListener(SwipeRefreshLayout.OnRefreshListener onRefreshListener) {
+        swipeRefreshLayout.setOnRefreshListener(onRefreshListener);
+    }
+
+    public void addOnItemTouchListener(@NonNull RecyclerView.OnItemTouchListener listener){
+        recyclerView.addOnItemTouchListener(listener);
+    }
+
+    public void setRefreshing(boolean refreshing) {
+        swipeRefreshLayout.setRefreshing(refreshing);
+
+        if(!refreshing) {
+            Animation in = AnimationUtils.loadAnimation(context, R.anim.fadeout_lento);
+            goToTopImageView.startAnimation(in);
+            goToTopImageView.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void fadeIn(View button) {
+        Animation in = AnimationUtils.loadAnimation(context, android.R.anim.fade_in);
+        button.startAnimation(in);
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static class ItemClickListener implements RecyclerView.OnItemTouchListener {
+
+        private Listener mListener;
+        GestureDetector mGestureDetector;
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            View childView = rv.findChildViewUnder(e.getX(), e.getY());
+            if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
+                mListener.onItemClick(childView, rv.getChildAdapterPosition(childView));
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+
+        public ItemClickListener(Listener listener) {
+            mListener = listener;
+            mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && mListener != null) {
+                        mListener.onLongItemClick(child, recyclerView.getChildAdapterPosition(child));
+                    }
+                }
+            });
+
+        }
+    }
+
+    public interface Listener extends AdapterView.OnItemClickListener {
+        public void onItemClick(View view, int position);
+        public void onLongItemClick(View view, int position);
+    }
+}
+
