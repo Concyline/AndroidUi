664993c6b6955596d7324c1ec37cc9e67e9c186c
aureo <concyline@hotmail.com>
2021-05-17 16:20:52 -0300

    soap action return

---
+++ httpagent/src/main/java/br/com/httpagent/soapaction/JSoapClass.java
@@ -0,0 +1,12 @@
+package br.com.httpagent.soapaction;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface JSoapClass {
+    String namespace();
+}
