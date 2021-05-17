32cfec34e8e3f1e5f7a9e92870e6199951e1546a
aureo <concyline@hotmail.com>
2021-05-17 15:26:53 -0300

    correcao

--- httpagent/src/main/java/br/com/httpagent/soapaction/JSoapCallback.java
+++
@@ -1,7 +0,0 @@
-package br.com.httpagent.soapaction;
-
-public abstract class JSoapCallback {
-    public abstract void onSuccess(Object result);
-    public abstract void onError(int error);
-    public void onDebugMessage(String title, String message) {}
-}
